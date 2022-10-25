7b9dca3ab5145c7f0c2bcdf9978a9de915aad237
Illia Yushkovskyi <haishtipil@gmail.com>
2022-10-25 10:27:39 +0200

    Добавьте файлы проекта.

---
+++ OnlyNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace OnlyNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String String = "./DOC1/XMLFile1.xml";
+            XmlTextReader reader = new XmlTextReader(String);
+
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        Console.Write("<" + reader.Name);
+
+                        while (reader.MoveToNextAttribute())
+                            Console.Write(" " + reader.Value + "'");
+                        Console.Write(">");
+                        Console.WriteLine(">");
+                        break;
+                    case XmlNodeType.Text:
+                        Console.WriteLine();
+                        break;
+                    case XmlNodeType.EndElement:
+                        Console.WriteLine();
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ xml/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace xml
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            String String = "./DOC/XMLFile1.xml";
+            XmlTextReader reader = new XmlTextReader(String);
+
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element: 
+                        Console.Write("<" + reader.Name);
+
+                        while (reader.MoveToNextAttribute()) 
+                            Console.Write(" " + reader.Name + "='" + reader.Value + "'");
+                        Console.Write(">");
+                        Console.WriteLine(">");
+                        break;
+                    case XmlNodeType.Text: 
+                        Console.WriteLine(reader.Value);
+                        break;
+                    case XmlNodeType.EndElement:
+                        Console.Write("</" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
